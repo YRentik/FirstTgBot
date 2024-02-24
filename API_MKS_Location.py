222e51eca2e46ce2edde8da37a142e95a72dc4a2
Renat Yaparov <rentik0987@yandex.ru>
2024-02-24 07:07:26 +0300

    test

--- API_MKS_Location.py
+++ API_MKS_Location.py
@@ -1,14 +1,3 @@
-'''from dataclasses import dataclass
-
-@dataclass
-class User:
-    user_id: int
-    name: str
-    age: int
-    email:str
-dp = User.user_id = 12
-print(dp)
-'''
 import requests
 
 api_url = 'http://api.open-notify.org/iss-now.json'
