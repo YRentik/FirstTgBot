979edbad035062c54aeffb3dfa4d5fc1a33d8ff3
Renat Yaparov <rentik0987@yandex.ru>
2024-02-24 19:32:21 +0300

    random cats image in tg bots

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-'''from dataclasses import dataclass
 
-@dataclass
-class User:
-    user_id: int
-    name: str
-    age: int
-    email:str
-dp = User.user_id = 12
-print(dp)
-'''
 import requests
+import time
 
-api_url = 'http://api.open-notify.org/iss-now.json'
+API_URL = 'https://api.telegram.org/bot'
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+BOT_TOKEN = '7023815986:AAGqZLAIqYsFG30fsN2FLvVka8EPGK6mCNY'
+TEXT = 'Ураааа!!! я работаю почти автоматически ыгыггыгыыгыг'
+ERROR_TEXT = 'Здесь должна быть картинка с котиком'
 
-response = requests.get(api_url)
+offset = -2
+counter = 0
+cat_response: requests.Response
+cat_link: str
 
-if response.status_code == 200:
-    print(response.text)
-else:
-    print(response.status_code)
+while counter < 100:
+    print('attempt =', counter)
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+
+    time.sleep(1)
+    counter += 1
