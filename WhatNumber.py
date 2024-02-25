8d9315ca418b6a2f9fdac99169d1aa2f7a0d1682
Renat Yaparov <rentik0987@yandex.ru>
2024-02-25 06:29:02 +0300

    mini game "What's for number"

--- WhatNumber.py
+++ WhatNumber.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 
+BOT_TOKEN = '7023815986:AAGqZLAIqYsFG30fsN2FLvVka8EPGK6mCNY'
 
 def get_random_number() -> int:
     return random.randint(1, 100)
